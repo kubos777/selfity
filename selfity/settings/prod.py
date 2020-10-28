5b80f1d078b4885b8552b4c0de756b6dc24c8847
kubos777 <jorgechavez.proteco@gmail.com>
2020-10-28 16:20:47 -0600

    feat: removing env from repo, prod file added

---
+++ selfity/settings/prod.py
@@ -0,0 +1,26 @@
+from .base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        "CLIENT": {
+           "name": 'selfity',
+           "host": 'mongodb+srv://jchavez:kub0s911@cluster0.sqozq.mongodb.net/selfity?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority',
+           "username": 'jchavez',
+           "password": 'kub0s911',
+           "authMechanism": "SCRAM-SHA-1",
+        }, 
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
